512c56787f0802e36665288a60edffee24bbbacb
Krushna Avhad <krushna07avhad@gmail.com>
2026-03-17 13:40:38 +0530

    Deploy frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 //const BASE_URL = 'https://carbonfootprinttracker-backend.onrender.com/api';
 //const BASE_URL = process.env.BASE_URL || 'http://localhost:5000/api';
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+console.log('API URL:', BASE_URL); // remove after testing
 
 const API = axios.create({
   baseURL: BASE_URL,
